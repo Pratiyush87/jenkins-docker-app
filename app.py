2d0507766deb5ca443363c2c962d35fc06c740d0
Pratiyush87 <raajpratiyush81028@gmail.com>
2026-04-17 18:47:06 +0530

    test webhook again

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "My name is Pratiyush Raj and I Deployed a file using Jenkins + Docker + GitHub"
+    return "My name is Pratiyush Raj and I Deployed a file using Jenkins + Docker + GitHub My friend is doing devops"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
