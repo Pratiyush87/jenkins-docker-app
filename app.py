b425c5e7741c247b00b1eeba4693e31ed9512452
Pratiyush87 <111338545+Pratiyush87@users.noreply.github.com>
2026-04-20 18:58:24 +0530

    Update home route response message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "My name is Pratiyush Raj and I Deployed a file using Jenkins + Docker + GitHub My friend is doing devops aman is doing devops and he is really a genius.And i am also a genius.We both are genius"
+    return "My name is Pratiyush Raj and I Deployed a file using Jenkins + Docker + GitHub My friend is doing devops aman is doing devops and he is really a genius.And i am also a genius.We both are genius we are doing devops"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8081)
